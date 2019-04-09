9cd2e69a1aed24d387bb931ed7240262ec3c9a83
Michal Mojek <m.mojek@gmail.com>
2019-04-09 11:59:37 +0200

    Add fetch data for runner with tests

--- running_results_fetcher/running_results_fetcher.py
+++ running_results_fetcher/running_results_fetcher.py
@@ -1,5 +1,6 @@
 from .spider_runner import SpiderRunner
 from .spider import Spider
+from .enduhube_page_parser import EnduhubPageParser
 # from .enduhube_page_parser import EnduhubPageParser
 
 
@@ -7,7 +8,6 @@ class RunningResultFetcher:
     """This class manage all functionality of the package"""
 
     def __init__(self, **kwargs):
-        self.data_downloaded = False
         self.spider_config = kwargs.get('spider_config')
 
     def set_spider_config(self, spider_config):
@@ -15,11 +15,10 @@ class RunningResultFetcher:
 
     def fetch_data_for_runner(self):
         spider = SpiderRunner(Spider, self.spider_config)
+        runner = self.spider_config.runner
         raw_pages = spider.download_raw_pages()
-        # TODO extrackt results and add to runner
         for raw_page in raw_pages:
-            pass
-            # page = EnduhubPageParser(raw_page)
-            # list_of_races = page.parse_page()
-
-        self.data_downloaded = True
+            page = EnduhubPageParser(raw_page)
+            races = page.parse_page()
+            runner.add_races(races)
+        return runner
--- tests/test_running_results_fetcher.py
+++ tests/test_running_results_fetcher.py
@@ -4,31 +4,16 @@
 """Tests for `running_results_fetcher` package."""
 
 from unittest.mock import patch
-from click.testing import CliRunner
-
-
-from running_results_fetcher import cli
-
-
-def test_command_line_interface():
-    """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'running_results_fetcher.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
 
 
 @patch('running_results_fetcher.running_results_fetcher.'
        'SpiderRunner.download_raw_pages')
-def test_fetch_data_and_download_data_to_true(SpiderRunnerMock, rrf,
-                                              spider_config, raw_page_html):
+def test_fetch_data(download_raw_pages_mock, rrf, spider_config, raw_page_html):
+    runner = spider_config.runner
+    download_raw_pages_mock.return_value = [raw_page_html]
     rrf.set_spider_config(spider_config)
-    # TODO monkey patching do right test
     rrf.fetch_data_for_runner()
-    assert rrf.data_downloaded is True
+    assert runner.race_results[0].race_name == 'V Bieg Niepodległości'
 
 
 # def test_fetch_data_for_runner(rrf, spider_config):
