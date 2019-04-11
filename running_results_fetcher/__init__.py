4c19d079099b3bb3de3019a0ee25de8ef0113301
Michal Mojek <m.mojek@gmail.com>
2019-04-11 09:43:40 +0200

    update init

--- running_results_fetcher/__init__.py
+++ running_results_fetcher/__init__.py
@@ -5,3 +5,9 @@
 __author__ = """Michal Mojek"""
 __email__ = 'm.mojek@gmail.com'
 __version__ = '0.1.0'
+
+
+from .app import RunningResultFetcher
+
+# print(RunningResultFetcher)
+__all__ = ['RunningResultFetcher']
