88a7f96583a68751d07f5d8653291b7c7d8fdfba
Michal Mojek <m.mojek@gmail.com>
2019-04-12 12:11:57 +0200

    ver 0.2.2

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mojek/running_results_fetcher',
-    version='0.2.1',
+    version='0.2.2',
     zip_safe=False,
 )
